f57337ca1086502e74e7ecfbdbbc0b7352f9190c
Germain kataku <katakugermain@gmail.com>
2020-12-02 08:12:23 +0100

    error fix

--- app/views.py
+++ app/views.py
@@ -33,15 +33,11 @@ def contact_view(request):
         email_id = request.POST['email_id']
         phone_num = request.POST['phone_num']
         message = request.POST['message']
-        #print(first_name,last_name,objet_name,email_id,phone_num,message)
-        if len(first_name)<3 or len(last_name)<3 or len(email_id)<5 or len(phone_num)<10 or len(message)<10:
-            messages.error(request,'Svp, remplissez les champs correctement')
-        else:
-            contact_us = ContactForm(first_name=first_name,last_name=last_name,objet_name=objet_name,email_id=email_id,phone_num=phone_num,message=message)
-            contact_us.save()
-            # send_mail(first_name, )
-            messages.success(request, 'Merci! Nous avons réçu votre message')
-            return redirect('/contact')
+        contact_us = ContactForm(first_name=first_name,last_name=last_name,objet_name=objet_name,email_id=email_id,phone_num=phone_num,message=message)
+        contact_us.save()
+        # send_mail(first_name, )
+        messages.success(request,'! Nous avons réçu votre message')
+        return redirect('/contact')
 
     template_name = 'pages/app/contact.html'
     return render(request, template_name, context=None)
