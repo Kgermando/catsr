54bce30aee8f614462788e6fe34d406af656e598
Germain kataku <katakugermain@gmail.com>
2021-05-13 00:45:44 +0200

    add keys

--- catsr/settings.py
+++ catsr/settings.py
@@ -202,14 +202,14 @@ if DEBUG == True:
 
 
 else:
-    AWS_ACCESS_KEY_ID = '3W64POGHLZJESXQRCUXO'
-    AWS_SECRET_ACCESS_KEY = '+MQS0GYjtkcMfaiMwEubGC0XSShjulKVjI8OxftwP/I'
+    AWS_ACCESS_KEY_ID = 'IBKD3NNQFIG4DCLPXNM6'
+    AWS_SECRET_ACCESS_KEY = 'vU0b9MGooFq3mBSv8JNgCuQhsgeNijrEp5OIUV8o874'
     AWS_STORAGE_BUCKET_NAME = 'catsr-space'
     AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
-    AWS_LOCATION = 'catsr-space-static'
+    AWS_LOCATION = 'catsr-static'
 
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
